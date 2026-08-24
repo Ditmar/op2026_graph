3ef776a5563bf8ca1da1ebf10ecf8cb2a2e3d6ae
ditmarcastro@gmail.com <ditmarcastro@gmail.com>
2026-08-24 09:10:29 -0400

    feat: remove prim

--- graphs/src/graph/Graph.ts
+++ graphs/src/graph/Graph.ts
@@ -63,41 +63,6 @@ class Graph {
             }
         }
     }
-    private prim(): void {
-        const noConnectedList: Vertex[] = [];
-        const connectedList: Vertex[] = [];
-        for (const vertex of this.vertexMap.values()) {
-            noConnectedList.push(vertex);
-        }
-        const startVertex = noConnectedList.shift();
-        if (!startVertex) {
-            console.log("Graph is empty.");
-            return;
-        }
-        connectedList.push(startVertex);
-        while (noConnectedList.length > 0) {
-            let minEdge: Edge | null = null;
-            let minWeight = Infinity;
-            for (const connectedVertex of connectedList) {
-                for (const edge of connectedVertex.getEdges()) {
-                    const neighbor = edge.getDestination();
-                    if (noConnectedList.includes(neighbor) && edge.getWeight() < minWeight) {
-                        minWeight = edge.getWeight();
-                        minEdge = edge;
-                    }
-                }
-            }
-            if (minEdge) {
-                const neighbor = minEdge.getDestination();
-                connectedList.push(neighbor);
-                noConnectedList.splice(noConnectedList.indexOf(neighbor), 1);
-                console.log(`Added edge from ${minEdge.getSource().getLabel()} to ${neighbor.getLabel()} with weight ${minEdge.getWeight()}`);
-            } else {
-                console.log("No more edges to add. Graph may be disconnected.");
-                break;
-            }
-        }
-        
-    }
+    
 }
 export default Graph;
